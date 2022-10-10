6c0895381010360fe93b7b460b4fe81681064599
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-10-10 14:41:32 +0100

    create components

---
+++ client/src/components/MyHousework.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MyHouseWork() {
+  return <div>This is My Housework Page</div>;
+}
+
+export default MyHouseWork;
---
+++ client/src/components/MyTools.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MyTools() {
+  return <div>This is My Tools Page</div>;
+}
+
+export default MyTools;
