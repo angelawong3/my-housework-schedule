a0da0d8bac42e7c7a15d467efcf7dbbe8b206d4e
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-10-11 21:26:27 +0100

    basic html & css for homepage

--- client/src/components/Homepage/Homepage.js
+++ client/src/components/Homepage/Homepage.js
@@ -1,8 +1,24 @@
 import React from "react";
+import "./Homepage.css";
 // import { useState, useEffect } from "react";
 
 function Homepage() {
-  return <div>This is Homepage</div>;
+  return (
+    <>
+      <div className="welcome-msg">WELCOME MESSAGE</div>
+      <div className="homepage-weather">
+        {/* search for current weather in user's city */}
+        <div className="search-area">
+          <div className="search-bar">
+            <input type="text" placeholder="Enter your city" />
+            <button>Search</button>
+          </div>
+        </div>
+        {/* render the weather information */}
+        <div className="render-weather">Here to render weather info</div>
+      </div>
+    </>
+  );
 }
 
 export default Homepage;
