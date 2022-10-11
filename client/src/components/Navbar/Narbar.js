4c2534688204c1dbf16a5115485db23afbe24c06
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-10-11 20:08:50 +0100

    responsive navbar done, tested

--- client/src/components/Navbar/Narbar.js
+++ client/src/components/Navbar/Narbar.js
@@ -2,7 +2,25 @@ import React from "react";
 import "./Navbar.css";
 
 function Navbar() {
-  return <nav>nav</nav>;
+  return (
+    <section class="top-nav">
+      <div>My Housework APP</div>
+      <input id="menu-toggle" type="checkbox" />
+      <label className="menu-button-container" for="menu-toggle">
+        <div className="menu-button"></div>
+      </label>
+      <ul className="menu">
+        {/* public */}
+        <li>Homepage</li>
+        <li>Login</li>
+        <li>Signup</li>
+        {/* private */}
+        <li>My Houserwork</li>
+        <li>My Tools</li>
+        <li>Logout</li>
+      </ul>
+    </section>
+  );
 }
 
 export default Navbar;
