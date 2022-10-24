75d72aab1c947dd94b0548cb8a3dbce8d6ccfda4
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-10-24 14:01:02 +0100

    work with ApolloClient

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,54 @@ import Wrapper from "./components/Wrapper/Wrapper";
 import MyHouseWork from "./components/MyHousework/MyHousework";
 import MyTools from "./components/MyTools/MyTools";
 import Login from "./components/Login/Login";
+
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import "./App.css";
+
+const httpLink = createHttpLink({
+  // uri: `${window.location.origin}/graphql`,
+  uri: "http://localhost:3001/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Wrapper>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/myhousework" element={<MyHouseWork />} />
-          <Route path="/mytools" element={<MyTools />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="*" element={<h1>404: Not Found</h1>} />
-        </Routes>
-      </Wrapper>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <Navbar />
+        <Wrapper>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/myhousework" element={<MyHouseWork />} />
+            <Route path="/mytools" element={<MyTools />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<h1>404: Not Found</h1>} />
+          </Routes>
+        </Wrapper>
+      </Router>
+    </ApolloProvider>
   );
 }
 
