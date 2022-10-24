1877a3393df1efd76a9e4c106510540066468fd8
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-10-24 14:01:55 +0100

    login function done

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 
+import { useMutation } from "@apollo/client";
+import Auth from "../../utils/auth";
+import { LOGIN } from "../../utils/mutations";
+
 function Login() {
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [login] = useMutation(LOGIN);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    try {
+      const { data } = await login({
+        variables: {
+          email: loginData.email,
+          password: loginData.password,
+        },
+      });
+
+      Auth.login(data.login.token);
+    } catch (err) {
+      console.error(err);
+    }
+
+    setLoginData({
+      email: "",
+      password: "",
+    });
+  };
+
   return (
     <div className="login-container">
       <div className="login-pic">
@@ -13,16 +47,37 @@ function Login() {
       <div className="login-form">
         <form>
           <div className="login-form-input">
-            <input type="email" name="email" id="email" placeholder="Email" />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email"
+              value={loginData.email}
+              onChange={(event) => {
+                setLoginData({
+                  ...loginData,
+                  email: event.target.value,
+                });
+              }}
+            />
             <br />
             <input
               type="password"
               name="password"
               id="password"
               placeholder="Password"
+              value={loginData.password}
+              onChange={(event) => {
+                setLoginData({
+                  ...loginData,
+                  password: event.target.value,
+                });
+              }}
             />
             <br />
-            <button type="submit">Login</button>
+            <button type="submit" onSubmit={handleLogin}>
+              Login
+            </button>
           </div>
         </form>
       </div>
