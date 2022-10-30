362ebab85a4509ff6670f55a027ce9af872a079b
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-10-30 17:40:04 +0000

    a form for user to input housework

--- client/src/components/MyHousework/MyHousework.js
+++ client/src/components/MyHousework/MyHousework.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import "./MyHousework.css";
+
+// import { useQuery, useMutation } from "@apollo/client";
+// import { QUERY_HOUSEWORK } from "../utils/queries";
+// import { ADD_HOUSEWORK, EDIT_HOUSEWORK, DEL_HOUSEWORK } from "../utils/mutations";
 
 function MyHouseWork() {
-  return <div>This is My Housework Page</div>;
+  return (
+    <>
+      <form>
+        <input type="text" placeholder="Housework Name" />
+        <input type="text" placeholder="Description" />
+        <select>
+          <option value="daily">Daily</option>
+          <option value="weekly">Weekly</option>
+          <option value="monthly">Monthly</option>
+          <option value="yearly">Yearly</option>
+        </select>
+        <button type="submit">Add Housework</button>
+      </form>
+    </>
+  );
 }
 
 export default MyHouseWork;
