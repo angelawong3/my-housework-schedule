3b39c87742c75059ce2b1b02eb2656ab7e935ff9
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-11-02 23:11:30 +0000

    change housework to an array for map function

--- server/models/Housework.js
+++ server/models/Housework.js
@@ -3,25 +3,29 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const houseworkSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  frequency: {
-    type: String,
-    enum: ["daily", "weekly", "monthly", "yearly"],
-    default: "weekly",
-    required: true,
-  },
   user: {
     type: Schema.Types.ObjectId,
     ref: "User",
   },
+  housework: [
+    {
+      name: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      description: {
+        type: String,
+        required: true,
+      },
+      frequency: {
+        type: String,
+        enum: ["daily", "weekly", "monthly", "yearly"],
+        default: "weekly",
+        required: true,
+      },
+    },
+  ],
 });
 
 const Housework = mongoose.model("Housework", houseworkSchema);
